1e78dd3b719e3e7b55af1b685da90a832dfe67a4
WakasaHitomi <31477539+WakasaHitomi@users.noreply.github.com>
2017-09-01 08:53:18 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,17 @@
+# This program says hello and greets a person by name.
+#
+#Your name
+#August 31, 2017
+print("Hello.")
+print("What is your name?")
+name =input()
+print("It is good to meet you, " + name + ".")
+print("Where were you born, " + name + "?")
+state =input()
+print("That's interesting. I'd like to visit " + state + " one day.")
+print("What is it like there?")
+condition =input()
+print( condition + "? That doesn't seem like I'd like to be stuck there. I changed my mind, seems boring and cliche.")
+print("I'm done talking with you, goodbye.")
+reply =input()
+print("I'm just gonna ignore you now")
