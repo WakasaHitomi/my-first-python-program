16f7a7105743623723446989cc09bfb7713ff860
WakasaHitomi <31477539+WakasaHitomi@users.noreply.github.com>
2017-09-05 09:30:11 -0400

    Add files via upload

--- geometry.py
+++ geometry.py
@@ -16,3 +16,21 @@ def circle_area(r):
 print(triangle_area(4,9))
 print(circle_area(5))
 print(circle_area(12))
+
+# my attempt
+
+def parallelogram_area(b, h):
+    a = b * h
+    return a
+def trapezoid_area(x, b, h):
+    a = ((x + b)/2) * h
+    return a
+def rectengular_prism_volume(w, h, l):
+    v = w * h * l
+    return v
+def cone_volume(r, h):
+    v = math.pi * r**2 * (h/3)
+    return v
+def sphere_volume(r):
+    v = (4/3) * math.pi * r**3
+    return v
